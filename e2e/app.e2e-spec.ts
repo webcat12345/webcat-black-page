d3f93966e3c92609a8376bafbcdad9e18a7d4c9e
webcat12345 <webcat91@gmail.com>
2017-03-19 04:25:33 +0100

    feature # - figure out e2e testing * figure out end to end testing with jasmin * travis script update

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('webcat-black App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect<any>(page.getParagraphText()).toEqual('header works!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class WebcatBlackPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root app-header p')).getText();
   }
 }
