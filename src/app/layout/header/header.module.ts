2bdd32a484efb70ca5d3d73cd81eefc87c69802e
webcat12345 <webcat91@gmail.com>
2017-04-13 18:29:20 +0200

    Feature #1 - fixed navigation bar constructing * notification dropdown

--- src/app/layout/header/header.module.ts
+++ src/app/layout/header/header.module.ts
@@ -1,16 +1,20 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
 import { HeaderComponent } from './header/header.component';
+import { NotificationTogglerComponent } from './notification-toggler/notification-toggler.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TooltipModule.forRoot()
   ],
   exports: [
     HeaderComponent
   ],
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, NotificationTogglerComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HeaderModule { }
