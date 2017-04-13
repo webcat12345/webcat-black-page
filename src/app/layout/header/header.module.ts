15993950bba4ca1df316dc15d204702c58ad9c0f
webcat12345 <webcat91@gmail.com>
2017-04-13 20:30:44 +0200

    Feature #1 - fixed navigation bar constructing * navbar finishing

--- src/app/layout/header/header.module.ts
+++ src/app/layout/header/header.module.ts
@@ -5,6 +5,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { HeaderComponent } from './header/header.component';
+import { NavbarSearchFormComponent } from './navbar-search-form/navbar-search-form.component';
 import { NavbarUserMenuComponent } from './navbar-user-menu/navbar-user-menu.component';
 import { NotificationTogglerComponent } from './notification-toggler/notification-toggler.component';
 
@@ -17,7 +18,7 @@ import { NotificationTogglerComponent } from './notification-toggler/notificatio
   exports: [
     HeaderComponent
   ],
-  declarations: [HeaderComponent, NotificationTogglerComponent, NavbarUserMenuComponent],
+  declarations: [HeaderComponent, NotificationTogglerComponent, NavbarUserMenuComponent, NavbarSearchFormComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HeaderModule { }
